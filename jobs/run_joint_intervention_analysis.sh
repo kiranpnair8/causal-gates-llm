ba7cce558f0a9c17fa0805d6b18366412e64513f
Kiran P Nair <kiranpnair65@gmail.com>
2026-09-22 16:30:24 -0500

    gpuno update

--- jobs/run_joint_intervention_analysis.sh
+++ jobs/run_joint_intervention_analysis.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/joint_intervention_%j.err
 #SBATCH --time=12:00:00
 #SBATCH --partition=gpu
-#SBATCH --nodelist=gpu002
+#SBATCH --nodelist=gpu004
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=48G
